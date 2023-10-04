9dc1a3a893fde2bceabc1e3beaaa8774ea980b71
Parth Bhattacharya <parthget@gmail.com>
2023-10-04 20:35:14 +0530

    add dashboard page and schema

---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+
+const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(visible) => {
+        if (!visible) {
+          setIsOpen(visible);
+        }
+      }}
+    >
+      <DialogTrigger onClick={() => setIsOpen(true)} asChild>
+        <Button>Upload PDF</Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <div className="">To Do</div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default UploadButton;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,8 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { publicProcedure, router } from "./trpc";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
+import { z } from "zod";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -19,7 +20,6 @@ export const appRouter = router({
 
     if (!dbUser) {
       // create user in db
-      console.log("no user");
       await db.user.create({
         data: {
           id: user.id,
@@ -30,6 +30,39 @@ export const appRouter = router({
 
     return { success: true };
   }),
+
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx;
+
+    return await db.file.findMany({
+      where: {
+        userId,
+      },
+    });
+  }),
+
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return file;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
