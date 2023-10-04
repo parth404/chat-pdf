abe9b07c5a1daed0e718108086c19df6359cc88a
Parth Bhattacharya <parthget@gmail.com>
2023-10-04 21:40:11 +0530

    add file page and setup pdf renderer

---
+++ src/components/ChatWrapper.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {
+  isSubscribed: boolean;
+  fileId: string;
+};
+
+const ChatWrapper = ({ isSubscribed, fileId }: Props) => {
+  return <div>ChatWrapper</div>;
+};
+
+export default ChatWrapper;
