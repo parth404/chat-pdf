a02a8cd52bcea79c9d1bc2bd2eb5f6b9ca893af5
Parth Bhattacharya <parthget@gmail.com>
2023-10-06 16:47:01 +0530

    fix multiple explorer tab issue

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -90,10 +90,7 @@ const UploadDropzone = ({ isSubscribed }: { isSubscribed: boolean }) => {
           className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"
         >
           <div className="flex items-center justify-center h-full w-full">
-            <label
-              htmlFor="dropzone-file"
-              className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
-            >
+            <div className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
               <div className="flex flex-col items-center justify-center pt-5 pb-6">
                 <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
                 <p className="mb-2 text-sm text-zinc-700">
@@ -140,7 +137,7 @@ const UploadDropzone = ({ isSubscribed }: { isSubscribed: boolean }) => {
                 id="dropzone-file"
                 className="hidden"
               />
-            </label>
+            </div>
           </div>
         </div>
       )}
